8c997849ee28f8cf7128990b038f4a1ee88c6a23
Miskolczi Richárd <miskolczi.richard@protonmail.com>
2026-04-10 16:16:10 +0200

    Adjusted project to better accomodate for non-FHS-compliant systems

--- include/Macros.hpp
+++ include/Macros.hpp
@@ -1,9 +1,9 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define HYPRCTL_BINARY "/usr/bin/hyprctl"
+#define HYPRCTL_BINARY "hyprctl"
 #define NULL_PATH "/dev/null"
-#define ECHO_PATH "/usr/bin/echo"
-#define CAT_PATH "/usr/bin/cat"
+#define ECHO_PATH "echo"
+#define CAT_PATH "cat"
 
 #endif
