16da7f6beb4c09ba98a0ab96def378c52f89f49c
Miskolczi Richárd <miskolczi.richard@protonmail.com>
2026-06-29 14:33:09 +0200

    Flake refinements

--- flake.nix
+++ flake.nix
@@ -3,18 +3,45 @@
 	
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    		flake-utils.url = "github:numtide/flake-utils";
 	};
 	
-	outputs = { self, nixpkgs }:
-	let
-		system = "x86_64-linux";
-		pkgs = nixpkgs.legacyPackages.${system};
-	in
-	{
-		packages.${system}.default = pkgs.callPackage ./default.nix {};
-		apps.${system}.default = {
-			type = "app";
-			program = "${self.packages.${system}.default}/bin/htt";
-		};
-	};
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = nixpkgs.legacyPackages.${system};
+
+				hyprland-toggle-tiling = pkgs.stdenv.mkDerivation {
+					pname = "hyprland-toggle-tiling";
+					version = "latest";
+					
+					src = self;
+					
+					nativeBuildInputs = [
+						pkgs.cmake
+						pkgs.pkg-config
+					];
+					
+					buildInputs = [
+						pkgs.nlohmann_json
+					];
+					
+					cmakeFlags = [
+						"-DUSE_SYSTEM_JSON=ON"
+					];
+					
+					cmakeBuildType = "Release";
+					
+					meta = with pkgs.lib; {
+						description = "Toggle tiling and floating modes in Hyprland globally.";
+						homepage = "https://github.com/TypoMustakes/hyprland-toggle-tiling";
+						license = licenses.gpl3Only;
+						platforms = platforms.linux;
+						mainProgram = "htt";
+					};
+				};
+			in
+			{
+				packages.default = hyprland-toggle-tiling;
+			});
 }
